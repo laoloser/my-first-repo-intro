7f84c65d0b3cdee6eecf10956122d8bfc34b24f7
laoloser <slaux86@gmail.com>
2024-01-29 20:45:28 -0800

    xfer from codepen

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".logo").fadeIn(3000).delay(300).animate({opacity: "0.5"},2000);
+    $("#quarterlymenu").fadeIn(7000);
+    $("#locationAndHours").fadeIn(9000);
+    $(".mathsection").fadeIn(9000);
+  });
+  
